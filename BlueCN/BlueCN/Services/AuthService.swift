0209c9aceaef2358fea2e57715ad32a10041ad00
James Lemaire <lythonikjames@gmail.com>
2018-05-09 19:36:04 -0400

    new network api2

--- BlueCN/BlueCN/Services/AuthService.swift
+++ BlueCN/BlueCN/Services/AuthService.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+// To handle our API Data storage
+class AuthService {
+
+    //Singleton, to be accessible thoughout the app
+    //Can only have on single instance of it
+    static let instance = AuthService()
+    
+    //Default way to save User data
+    //(use for simple primitive data strings ect , no images or password)
+    let defaults = UserDefaults.standard
+    
+    var isLoggedIn : Bool {
+        
+        get {
+            return defaults.bool (fooKey: LOGGED_IN_KEY)
+        }
+        
+    }
+    
+
+
+}
