136e8b48e624a873788f9653c997bb665e43a69d
James Lemaire <lythonikjames@gmail.com>
2018-05-10 18:43:53 -0400

    added the persistent loging UI & functionalities

--- BlueCN/BlueCN/Controller/ChannelVC.swift
+++ BlueCN/BlueCN/Controller/ChannelVC.swift
@@ -12,7 +12,10 @@ class ChannelVC: UIViewController {
     
     //Outlets
     
+    @IBOutlet weak var userImg: CircleImage!
     @IBOutlet weak var loginBtn: UIButton!
+    
+    
     //Take us back to Channel View controller
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue) {}
    
@@ -22,13 +25,31 @@ class ChannelVC: UIViewController {
         //Enlarge the channel window view by 60% of the screen view
         self.revealViewController().rearViewRevealWidth =
             self.view.frame.size.width - 60
+        
+        //Add an Observer to listen to Notification(s)
+        //set in CreateAcountVC regarding user registration/login status
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDatadidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
     }
     
     //Perform segue action for the login button to show login View Controller
     @IBAction func loginBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: TO_LOGIN, sender: nil)
     }
-
-   
+    
+    @objc  func userDatadidChange(_ notif: Notification){
+        //if we logged in change btn title to current loggin name
+        if AuthService.instance.isLoggedIn {
+            loginBtn.setTitle(UserDataService.instance.name, for: .normal)
+            
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+           
+            userImg.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        } else{
+            loginBtn.setTitle("Login", for: .normal)
+            userImg.image = UIImage(named: "menuProfileIcon")
+            userImg.backgroundColor = UIColor.clear
+        }
+    }
+    
 
 }
--- BlueCN/BlueCN/Services/UserDataService.swift
+++ BlueCN/BlueCN/Services/UserDataService.swift
@@ -27,7 +27,7 @@ class UserDataService {
                      avatarName: String, email: String, name: String) {
         self.id = id
         self.avatarColor = color
-        self.avatarName = name
+        self.avatarName = avatarName
         self.email = email
         self.name = name
         
@@ -36,5 +36,45 @@ class UserDataService {
     //update the avatar name, we will need this
     func setAvatarName(avatarName: String){
         self.avatarName = avatarName
+    }
+    
+    //returning the UI Color, to display on channelvc
+    func returnUIColor(components: String) -> UIColor {
+        
+        //scanner to extract rgb value from color object in database
+        let scanner = Scanner(string: components)
+        //tell it which characters to skip
+        let skipped = CharacterSet(charactersIn: "[], ")
+        //start at the begining & stop at specific character
+        let comma = CharacterSet(charactersIn: ",")
+        //Characters to be skipped
+        scanner.charactersToBeSkipped = skipped
+        
+        //where to state the values
+        var r, g, b, a : NSString?
+        
+         scanner.scanUpToCharacters(from: comma, into: &r)
+         scanner.scanUpToCharacters(from: comma, into: &g)
+         scanner.scanUpToCharacters(from: comma, into: &b)
+         scanner.scanUpToCharacters(from: comma, into: &a)
+        
+        let defaultColor = UIColor.lightGray
+        
+        //Unwrapped the r, g, b, a values from optional
+        guard let rUnwrapped = r else { return defaultColor }
+        guard let gUnwrapped = g else { return defaultColor }
+        guard let bUnwrapped = b else { return defaultColor }
+        guard let aUnwrapped = a else { return defaultColor }
+        
+        //Covert the value into defaultValue, & then to a CGFloat
+        let rfloat = CGFloat(rUnwrapped.doubleValue)
+         let gfloat = CGFloat(gUnwrapped.doubleValue)
+         let bfloat = CGFloat(bUnwrapped.doubleValue)
+         let afloat = CGFloat(aUnwrapped.doubleValue)
+        
+        //Create the new UI Color
+        let newUIColor = UIColor(red: rfloat, green: gfloat, blue: bfloat, alpha: afloat)
+        
+        return newUIColor
     }
 }
